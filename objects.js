40fa458caa9a70a5ad806db9d953ff1b0a3e0ed1
Jonathan Lin <jlin8293@gmail.com>
2018-02-08 21:03:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,4 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle })
 }
 
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
